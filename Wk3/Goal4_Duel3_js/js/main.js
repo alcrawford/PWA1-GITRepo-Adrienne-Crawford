bb58bfb00a3035607ba444247c3f7d1184842ff7
alcrawford <adriennelorton@hotmail.com>
2015-08-01 19:06:30 -0400

    Week 4 Goal 7

--- Wk3/Goal4_Duel3_js/js/main.js
+++ Wk3/Goal4_Duel3_js/js/main.js
@@ -1,7 +1,7 @@
 /*
 Adrienne Crawford
 PWA1 
-Week 2, Duel 2
+Week 3, Duel 2
 July 17 2015
 */
 
@@ -10,8 +10,8 @@ July 17 2015
 
     console.log("FIGHT!!!");
 
-    var fighter1_txt = document.querySelector("#kabal").querySelector("p");
-    var fighter2_txt = document.querySelector("#kabal").querySelector("p");
+    var fighter1_txt = document.querySelector("#kabal").querySelector("#scores");
+    var fighter2_txt = document.querySelector("#kratos").querySelector("#scores");
     var round_txt = document.querySelector("h4");
     var button = document.getElementById("fight_btn");
 
@@ -24,7 +24,7 @@ July 17 2015
     var hero1 = names[0];
     var hero2 = names[1];               		 
 
-    player health & damage
+    player health & damage[{
     var healthDamage = [100, 100];				
     var heroHealth1 = healthDamage[0];
 	var heroHealth2 = healthDamage[1];
@@ -37,23 +37,70 @@ July 17 2015
 
     //new arrays w objects
 
-    var fighters
+    var fighters;
+    fighters = [
+        {
+            name: "Kabal",
+            damage: 20,
+            health: 100
+
+        },
+        {
+            name: "Kratos",
+            damage: 20,
+            health: 100
+
+        }];
 
     //initiate round
-    var round=0;						
+    var round = 1;
+
+    round_txt.innerHTML ="Click Fight Button to Start!";
+    fighter1_txt.innerHTML = fighters[0].name+ ":  " + fighters[0].health;
+    fighter2_txt.innerHTML = fighters[1].name+ ":  " + fighters[1].health;
 
     function fight(){
-        alert(playerOne[0]":"+playerOne[2]+"  *START*  "+playerTwo+":"+plaerTwo[2]);     /*prompt the stats and names of players to start*/
-        for (var i = 0; i < 10; i++){
-        
-            //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = playerOne[1] * .5;                                    /* minimum damage can be half of 20, max is 20 each round*/
+        /*old alert(playerOne[0]":"+playerOne[2]+"  *START*  "+playerTwo+":"+playerTwo[2]);
+        for (var i = 0; i < 10; i++){*/
+        //utilize new array with vars
+     fighter1_txt.innerHTML = fighters[0].name+ ":  " + fighters[0].health;
+     fighter2_txt.innerHTML = fighters[1].name+ ":  " + fighters[1].health;
+
+     var f1 = Math.floor(Math.random() * fighters[0].damage + fighters[0].damage *.5;
+     var f2 = Math.floor(Math.random() * fighters[1].damage + fighters[1].damage *.5;
+     fighters[0].health -= f1;
+     fighters[1].health -= f2;
+
+     var results = winnerCheck();
+     round_txt.innerHTML = "ROUND #" + round + "Results;";
+        round++;
+        if(results === "no winner"){
+            fighter1_txt.innerHTML = fighters[0].name + ":  " + fighters[0].health;
+            fighter2_txt.innerHTML = fighters[1].name + ":  " + fighters[1].health;
+        }else{
+            fighter1_txt.innerHTML = results;
+            fighter2_txt.innerHTML = "";
+
+            button.removeEventListener("click",fight,false );
+
+            document.querySelector(".buttonblue").innerHTML = "DONE!!!";
+        }
+    }
+
+    function winnerCheck(){
+        var results = "no winner";
+        if(fighters[0].health <1 && fighters [1].health <1){
+           results = "You Both Die - GAME OVER!";
+        }
+    }
+    /*old formula comment out random formula is - Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = playerOne[1] * .5;                                     minimum damage can be half of 20, max is 20 each round
             var minDamage2 = playerTwo[1] * .5;
-            var f1 = Math.floor(Math.random()*(playerOne[1])- minDamage1);  /* generate the random damage numbers between 10 and 20 for each player*/
-            var f2 = Math.floor(Math.random()*(playerTwo[1]- minDamage2 );  /*use formula with math.down to round down and math.random to generate number*/
+            var f1 = Math.floor(Math.random()*(playerOne[1])- minDamage1);   generate the random damage numbers between 10 and 20 for each player
+            var f2 = Math.floor(Math.random()*(playerTwo[1]- minDamage2 );  use formula with math.down to round down and math.random to generate number
 
             //inflict damage
-            playerOne[2]-=f1;                /*generate new health numbers for players using variables from prior formula*/
+            playerOne[2]-=f1;                generate new health numbers for players using variables from prior formula
             playerTwo[2]-=f2;
 
             console.log(playerOne[0]+":"+playerOne[2]+" "+playerTwo[0]+":"+playerTwo[2]);            
@@ -61,8 +108,8 @@ July 17 2015
             //check for victor
             var result = winnerCheck();
             console.log(result);
-            if (result==="no winner") /*if else statement that will work with if else below to determine */
-            {						  /* outcome. no match results in jump to program start or repeat at bottom */
+            if (result==="no winner") if else statement that will work with if else below to determine
+            {						   outcome. no match results in jump to program start or repeat at bottom
                 round++;
                 alert(playerOne[2]+":"+playerOne[2]+"  *ROUND "+round+" OVER*  "+playerTwo[0]+":"+playerTwo[2]);
 
@@ -76,13 +123,13 @@ July 17 2015
 
     function winnerCheck(){
         var result="no winner";
-        if (playerOne[2]<1 && playerTwo]<1) /*both players fall below 1 and no winner*/
+        if (playerOne[2]<1 && playerTwo]<1) /*both players fall below 1 and no winner
         {
             result = "You Both Die";
-        } else if(playerOne[0]<1){				/*one player falls below, 2 wins*/
+        } else if(playerOne[0]<1){				one player falls below, 2 wins
             result =playerTwo
             Name+" WINS!!!"
-        } else if (playerTwo[2]<1)				/*two player falls below, 1 wins*/
+        } else if (playerTwo[2]<1)				two player falls below, 1 wins
         {
             result =playerOne[0]+" WINS!!!"
         };
---
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Adrienne Crawford
+ * Class: PWA
+ * Goal: Goal7
+ */
+
