a5ad598bae3bebe279e26c72085d0487b18edbd3
alcrawford <adriennelorton@hotmail.com>
2015-08-02 14:14:04 -0400

    Goal 7 main.js Complete

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -1,6 +1,22 @@
 /**
- * Created by the JavaScript Development Team
+ * Created by Adrienne Crawford
  * Class: PWA
  * Goal: Goal7
  */
+
+//In sum, there would be three Constructors, along with 3 instances of Instantiation to go with them 
+and prototypes to detail them.
+
+//Name r1c1 would be constructor 1- the plans
+//Job r1c2 would be prototype to match above- add details
+//Action r1c3 would be instantiation to create object based off above parameters
+
+//Name r2c1 would be constructor 1- the plans
+//Job r2c2 would be prototype to match above- add details
+//Action r2c3 would be instantiation to create object based off above parameters
+
+//Name r3c1 would be constructor 1- the plans
+//Job r3c2 would be prototype to match above- add details
+//Action r3c3 would be instantiation to create object based off above parameters/**
+
 
